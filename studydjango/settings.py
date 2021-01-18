fb00cf3e964a6fa2b9d9ff7b8522408b6cb462af
杨坡 <000111aaaim@gmail.com>
2021-01-18 12:36:59 +0800

    impoer xadmin

--- studydjango/settings.py
+++ studydjango/settings.py
@@ -16,6 +16,8 @@ import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0,os.path.join(BASE_DIR,"apps"))
+sys.path.insert(0,os.path.join(BASE_DIR,"extra_app"))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -138,5 +140,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIR=[
     os.path.join(BASE_DIR,'static')
 ]
-MEDIA_URL='/static/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'/static/media')
+# MEDIA_URL='static/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/media')
